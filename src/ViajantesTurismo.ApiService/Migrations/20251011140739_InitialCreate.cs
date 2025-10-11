3065a8d23855723e54fa769634be80c9b0c32146
danigutsch <daniutsch@outlook.com>
2025-10-11 16:13:09 +0200

    Add initial migration for Tours table with relevant properties and indexes

---
+++ src/ViajantesTurismo.ApiService/Migrations/20251011140739_InitialCreate.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ViajantesTurismo.ApiService.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tours",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Identifier = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: false),
+                    Name = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    StartDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Price = table.Column<decimal>(type: "numeric", nullable: false),
+                    SingleRoomSupplementPrice = table.Column<decimal>(type: "numeric", nullable: false),
+                    RegularBikePrice = table.Column<decimal>(type: "numeric", nullable: false),
+                    EBikePrice = table.Column<decimal>(type: "numeric", nullable: false),
+                    IncludedServices = table.Column<string[]>(type: "text[]", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tours", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tours_Identifier",
+                table: "Tours",
+                column: "Identifier",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tours_Name",
+                table: "Tours",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tours");
+        }
+    }
+}
