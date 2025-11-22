e669fe3454e1cdc7739d3a8b6b5b791d79e04b51
danigutsch <daniutsch@outlook.com>
2025-11-22 09:06:39 +0100

    test: add unit test for retrieving customer with multiple bookings

--- tests/ViajantesTurismo.Admin.IntegrationTests/Customers/GetCustomerByIdTests.cs
+++ tests/ViajantesTurismo.Admin.IntegrationTests/Customers/GetCustomerByIdTests.cs
@@ -38,4 +38,28 @@ public sealed class GetCustomerByIdTests(ApiFixture fixture) : AdminApiIntegrati
         // Assert
         Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
     }
+
+    [Fact]
+    public async Task Can_Get_Customer_With_Bookings()
+    {
+        // Arrange
+        var customer = await Client.CreateTestCustomer("Customer", "WithBookings", TestContext.Current.CancellationToken);
+        var tour1 = await Client.CreateTestTour(cancellationToken: TestContext.Current.CancellationToken);
+        var tour2 = await Client.CreateTestTour("CUBA2025", "Cuba Adventure 2025", cancellationToken: TestContext.Current.CancellationToken);
+        await Client.CreateTestBooking(tour1.Id, customer.Id, cancellationToken: TestContext.Current.CancellationToken);
+        await Client.CreateTestBooking(tour2.Id, customer.Id, cancellationToken: TestContext.Current.CancellationToken);
+
+        // Act
+        var response = await Client.GetAsync(new Uri($"/customers/{customer.Id}", UriKind.Relative), TestContext.Current.CancellationToken);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        var customerDto = await response.Content.ReadFromJsonAsync<CustomerDetailsDto>(cancellationToken: TestContext.Current.CancellationToken);
+        Assert.NotNull(customerDto);
+        Assert.Equal(customer.Id, customerDto.Id);
+        var bookingsResponse = await Client.GetBookingsByCustomer(customer.Id, TestContext.Current.CancellationToken);
+        var bookings = await bookingsResponse.Content.ReadFromJsonAsync<GetBookingDto[]>(TestContext.Current.CancellationToken);
+        Assert.NotNull(bookings);
+        Assert.Equal(2, bookings.Length);
+    }
 }
