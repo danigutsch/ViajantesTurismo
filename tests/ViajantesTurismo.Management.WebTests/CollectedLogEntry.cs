d9c423a23eca00c842a9939c2917b3096ce0e0c3
Dani Giurizatto Utsch <daniutsch@outlook.com>
2026-07-11 16:14:02 +0200

    test(web): remove unused logger helpers

--- tests/ViajantesTurismo.Management.WebTests/CollectedLogEntry.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.Extensions.Logging;
-
-namespace ViajantesTurismo.Management.WebTests;
-
-internal sealed record CollectedLogEntry(
-    LogLevel LogLevel,
-    EventId EventId,
-    string Message,
-    IReadOnlyDictionary<string, string> State);
