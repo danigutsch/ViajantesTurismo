ed6f1dea334b99e902d81d710ce190826b988a30
Dani Giurizatto Utsch <daniutsch@outlook.com>
2026-07-11 13:50:54 +0200

    test(documents): make document store fake stateful

--- tests/ViajantesTurismo.Admin.UnitTests/Documents/FakeDocumentStore.cs
+++ tests/ViajantesTurismo.Admin.UnitTests/Documents/FakeDocumentStore.cs
@@ -8,13 +8,21 @@ internal sealed class FakeDocumentStore : IDocumentStore
 
     public Dictionary<Guid, DocumentDraft> Documents { get; } = [];
 
-    public void Add(DocumentDraft document) => AddedDocuments.Add(document);
+    public void Add(DocumentDraft document)
+    {
+        AddedDocuments.Add(document);
+        Documents.Add(document.Id, document);
+    }
 
     public Task<DocumentDraft?> GetById(Guid id, CancellationToken ct) =>
         Task.FromResult(Documents.GetValueOrDefault(id));
 
     public Task<IReadOnlyList<DocumentDraft>> GetExpiredDrafts(DateTime now, CancellationToken ct) =>
-        Task.FromResult<IReadOnlyList<DocumentDraft>>([]);
+        Task.FromResult<IReadOnlyList<DocumentDraft>>(Documents.Values.Where(document => document.IsExpiredDraft(now)).ToArray());
 
-    public void Remove(DocumentDraft document) => AddedDocuments.Remove(document);
+    public void Remove(DocumentDraft document)
+    {
+        AddedDocuments.Remove(document);
+        Documents.Remove(document.Id);
+    }
 }
